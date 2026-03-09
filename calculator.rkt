5f2767f66e96f602225250722619f8699a4b19fe
jubrilOlaniran <147342412+jubrilOlaniran@users.noreply.github.com>
2026-03-08 21:30:01 -0500

    Update calculator.rkt
    working on code

--- calculator.rkt
+++ calculator.rkt
@@ -8,3 +8,26 @@
 
 
 (define (tokenize s)
+  (define len (string-length s))
+
+  (define (skip-space i)
+    (cond
+      [(>= i len) i]
+      [(char-whitespace? (string-ref s i)) (skip-space (add1 i))]
+      [else i]))
+
+  (define (scan-number i)
+    (define (loop j)
+      (cond
+        [(>= j len) j]
+        [(char-numeric? (string-ref s j)) (loop (add1 j))]
+        [else j]))
+    (loop i))
+
+  (define (scan-history i)
+    (define start (add1 i))
+    (define end (scan-number start))
+    (if (= start end)
+        #f
+        (substring s i end)))
+
