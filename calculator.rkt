55720f7b56fe499e4198bbeb4c042eb89bbfa2a7
jubrilOlaniran <147342412+jubrilOlaniran@users.noreply.github.com>
2026-03-08 21:11:09 -0500

    Update calculator.rkt
    start calculator code with the tokenizer

--- calculator.rkt
+++ calculator.rkt
@@ -1,1 +1,10 @@
+#lang racket
+(require "mode.rkt")
 
+;; Prefix calculator with history.
+;; Runs in interactive mode when prompt? is true.
+;; Runs in batch mode when prompt? is false.
+;; Tokenizing
+
+
+(define (tokenize s)
